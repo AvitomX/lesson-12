51359ff649b7ff669df72bfa3ab4a94c8e586cce
AvitomX <aleksandr0409@gmail.com>
2020-04-01 18:36:06 +0300

    ToDoList created

---
+++ ToDoList/src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        //SpringApplication.run(Main.class, args);
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+    }
+}
